b7403448531297e0e274a23fe159400c6b1fca97
cathyqu <ranequ@gmail.com>
2024-11-03 13:20:07 -0500

    Co-authored-by: trigonal-bacon <trigonal-bacon@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -20,30 +20,65 @@ async def on_ready():
     synced_commands = await tree.sync(guild=discord.Object(id=GUILD_ID))
     print(f'Synced {len(synced_commands)} commands: {synced_commands}')
 
-@tree.command(name="cmwmc_schedule", description="cmwmc event schedule", guild=discord.Object(id=GUILD_ID))
+@tree.command(name="cmimc_schedule", description="cmimc event schedule", guild=discord.Object(id=GUILD_ID))
 async def slash_command(interaction: discord.Interaction):
-    await interaction.response.send_message('''```
-9:00       Breakfast/Check-in\n
-9:30       Opening ceremony\n
-10:00      Individual round/Speaker Talk\n
-11:15      Guts round\n
-12:30      Lunch\n
-1:30       Relay round\n
-2:30       Sponsor Mini-events\n
-4:30       Award ceremony```''')
+        await interaction.response.send_message('''
+**Friday**
+```
+4:00 pm | CMU Math Info @ Session Wean Hall 7500
 
-@tree.command(name="cmwmc_format", description="competition format", guild=discord.Object(id=GUILD_ID))
+6:00 pm	| First mini-event @ Doherty Hall and Wean Hall
+
+7:20 pm	| Second mini-event	@ Doherty Hall and Wean Hall
+
+8:30 pm | Early check-in @ Doherty Hall A302```
+
+**Saturday**
+```
+8:30 am  | Check-in @ Doherty Hall 1212, 1209
+
+9:30 am  | Opening ceremony @ Doherty Hall, University Center, and Wean Hall
+
+10:00 am | Team rounds @ Doherty Hall, University Center, and Wean Hall
+
+10:00 am | Coach talk with Po-Shen Loh @ Baker Hall A51
+
+12:00 pm | Lunch @ Baker Hall A53
+
+1:00 pm  | Individual rounds @ Doherty Hall, University Center, and Wean Hall
+
+5:00 pm	 | Award ceremony @ Doherty Hall, University Center, and Wean Hall```''')
+
+@tree.command(name="cmimc_rules", description="competition rules", guild=discord.Object(id=GUILD_ID))
 async def slash_command(interaction: discord.Interaction):
     await interaction.response.send_message('''
-**Individual Round**
-The individual round contains problems on Algebra, Number Theory, Geometry, Combinatorics, and Computer Science. 
+**General Rules**
+```A student may leave to use the restroom during the exam, but they must turn in their phone to a proctor to do so.
+No computational aids other than a writing utensil are allowed.
+If a student is caught cheating on an individual round, they will get a -X for that round.
+Where X is the score of the highest scoring individual on that round
+If a student is caught cheating on a team-based round, their team will get a -X for that round.
+Where X is the score of the highest scoring team on that round
+If a proctor has reasonable suspicion to believe that a student is cheating, the proctor reserves the right to declare cheating.
+Grading decisions (including TCS) can not be protested, but answer protests are welcome in the #protests-disputes channel on discord.```
+''')
 
-**Relay Round**
-Teams will work to solve sets of problems where each team member works on one problem of the set and passes their answer to the next team member, who will use that answer to solve their own problem, like a relay! No other communication is allowed. You don't have to wait for the person before you to finish before you start working on your problem. Only the last team member's submission will be graded.
+@tree.command(name="cmimc_format", description="competition format", guild=discord.Object(id=GUILD_ID))
+async def slash_command(interaction: discord.Interaction):
+    await interaction.response.send_message('''
+**TCS (Theoretical Computer Science) Round**
+This is a 60 minute test with 2-3 problems to be completed as a team. Each question is worth the same number of points. Communication within the team is permitted. Solutions should be written in english.
+
+**Team Round**
+This is a 40 minute test with 10 problems to be completed as a team. Each question is worth the same number of points. Communication within the team is permitted. Answers must be reasonably simplified.
 
 **Guts Round**
-Teams will work together to solve sets of problems. After completing one set, the next problem set will be given. Teams cannot return to edit answers from a previous set.        
+Teams will work together to solve sets of problems. After completing one set, the next problem set will be given. Teams cannot return to edit answers from a previous set. Communication within the team is permitted. Answers must be reasonably simplified.
 ''')
+
+@tree.command(name="website", description="link to website", guild=discord.Object(id=GUILD_ID))
+async def slash_command(interaction: discord.Interaction):    
+    await interaction.response.send_message("https://cmimc.math.cmu.edu/about")
 
 @tree.command(name="staff", description="staff contacts", guild=discord.Object(id=GUILD_ID))
 async def slash_command(interaction: discord.Interaction):    
